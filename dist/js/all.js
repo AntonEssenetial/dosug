80a70aa86c97a037e11ff9b7eba5aa5cb9b62db1
AntonEssential <antonessential@hotmail.co.uk>
2016-06-11 22:45:01 +0300

    version 1.0.0

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,9 +1,14 @@
+
 $(document).ready(function($) {
   // Mobile menu toggle function
   $('.toggle-menu').click(function(event) {
     $('.filters, .search__button-wrapper').removeClass('active');
     $(this).toggleClass('active');
     $('.main-menu').toggleClass('active animated fadeIn');
+  });
+  $('.button__sub').click(function(event) {
+    $('.button__sub').toggleClass('active')
+    $('.catalog-header__tabs').toggleClass('active')
   });
   $('.pseudo-dropdown_mobile .pseudo-dropdown__top').click(function(event) {
     $('.filters, .search__button-wrapper').removeClass('active');
@@ -26,11 +31,24 @@ $(document).ready(function($) {
       'width', $(window).width()
     );
   };
+  function heightDetect(){
+    $('.pseudo-dropdown_mobile .pseudo-dropdown__list').css( 
+      'height', $(window).height()
+    );
+  };
   widthDetect();
+  heightDetect();
   $(window).resize(function(){
     widthDetect();
+    heightDetect();
   });
   // close popup 
+  $('.b-popup-wrapper').mouseup(function (e) {
+    var container = $(".b-popup-wrapper");
+    if (container.has(e.target).length === 0){
+        container.removeClass('jsActive')();
+    }
+  });
   $('.popup__close').click(function(event) {
     $('.b-popup-wrapper').removeClass('jsActive')
   });
